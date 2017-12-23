9a2750fd63c49ae41a44576ace8c7018ddc468b3
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2017-12-23 16:14:40 -0700

    Set gameWon object based on winCombos

--- scripts.js
+++ scripts.js
@@ -58,6 +58,15 @@ $(document).ready(function() {
     let plays = board.reduce((a, e, i) =>
       (e === player) ? a.concat(i) : a, []);
     let gameWon = null;
+    for (let [index, win] of winCombos.entries()) {
+      if (win.every(elem => plays.indexOf(elem) > -1)) {
+        gameWon = {
+          index: index,
+          player: player
+        };
+        break;
+      }
+    }
   }
 
 }); //End document ready function
